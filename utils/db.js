7a4db0a38f492c3f4ba248d21a8cd0eb7c2cc92a
faroukbmiled <fcfaroukfc@gmail.com>
2023-04-27 23:10:24 +0100

    initial cmt => tsk 2

---
+++ utils/db.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    const url = `mongodb://${host}:${port}`;
+
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (err) console.log(err);
+      this.db = client.db(database);
+    });
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+module.exports = dbClient;
